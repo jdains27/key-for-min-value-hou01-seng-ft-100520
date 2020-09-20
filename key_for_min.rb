ad9723568b1491a5ab23ab2fdc702339d252fab0
Jordan Dains <dains.jordan2@gmail.com>
2020-09-20 03:42:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  name_hash.length 
+  if name_hash.index[0] < name_hash.index[1]
+    puts 
 end
 
- 
+#see how many elements are in the array use index to compare the min value of the one next to it //like a bracket until min value is left.
