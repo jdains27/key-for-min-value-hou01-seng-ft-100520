032800d7280792fdfb23629b040bc3d6fb870ba7
Jordan Dains <dains.jordan2@gmail.com>
2020-09-20 03:32:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-key_for_min_value(hash)
+def key_for_min_value(hash)
   end
 
   it "does not call the `#sort_by` method" do
