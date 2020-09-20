19d07f8643b5f8703532a97a1fbbbd7dee6e6952
Jordan Dains <dains.jordan2@gmail.com>
2020-09-20 03:51:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,22 +4,22 @@
 def key_for_min_value(name_hash)
   count = 0
   min_value = nil
-  least_key = nil
+  min_key = nil
   
   name_hash.each do |key, value|
     if count == 0 then
       min_value = value
-      least_key = key
+      min_key = key
     count += 1
       else
         if value < min_value then
           min_value = value
-          least_key = key
+          min_key = key
         end
       end
     end
 
-  return least_key
+  return min_key
 end
 
 
