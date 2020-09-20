0098056050389769bfaa0360e0233f2f6ecdf87a
Jordan Dains <dains.jordan2@gmail.com>
2020-09-20 03:33:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  end
 
   it "does not call the `#sort_by` method" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
